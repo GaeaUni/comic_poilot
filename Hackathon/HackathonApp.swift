f09a22ed03b9372ec15e6785a2c84f2d71d35237
shulong <shulong@kkworld.com>
2023-07-10 10:04:16 +0800

    Initial Commit

---
+++ Hackathon/HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonApp.swift
+//  Hackathon
+//
+//  Created by 周亮 on 2023/7/10.
+//
+
+import SwiftUI
+
+@main
+struct HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
