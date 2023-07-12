24f8c822041a49ea8fbacb0c10c8df386ab59157
shulong <shulong@kkworld.com>
2023-07-12 16:44:29 +0800

    feat: 增长mock的分镜数据生成预览图功能

--- Hackathon/StoryGeneratorView.swift
+++ Hackathon/StoryGeneratorView.swift
@@ -5,12 +5,15 @@
 //  Created by 周亮 on 2023/7/10.
 //
 
+import Alamofire
 import SwiftUI
 
 struct StoryGeneratorView: View {
     @State private var message = ""
     @State private var selectedShotCount = 1
     @State private var showShotList = false
+    @State private var shots: [ShotListView.Shot] = []
+    @State private var isLoading = false
 
     var body: some View {
         VStack(spacing: 16) {
@@ -38,22 +41,24 @@ struct StoryGeneratorView: View {
             }
             .padding()
 
-            Button(action: {
-                // 点击按钮的操作
-                showShotList = true
-            }) {
-                Text("生成分镜")
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.blue) // 设置按钮的背景颜色
-                    .cornerRadius(8)
+            if isLoading {
+                ProgressView()
+            } else {
+                Button(action: {
+                    // 点击按钮的操作
+                    generateShots()
+                }) {
+                    Text("生成分镜")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue) // 设置按钮的背景颜色
+                        .cornerRadius(8)
+                }
+                .sheet(isPresented: $showShotList) {
+                    ShotListView(shots: shots)
+                }
+                .padding()
             }
-            .sheet(isPresented: $showShotList) {
-                
-                ShotListView(shotCount: selectedShotCount)
-            }
-
-            .padding()
 
             Stepper(value: $selectedShotCount, in: 1 ... 20) {
                 Text("选择分镜数量: \(selectedShotCount)")
@@ -67,5 +72,26 @@ struct StoryGeneratorView: View {
                     .font(.headline)
             }
         }
+    }
+}
+
+extension StoryGeneratorView {
+    // 生成分镜
+    func generateShots() {
+        // 发送网络请求，获取分镜的数据
+        // 这里只是一个示例，你需要根据你的后端服务来实现这个功能
+        isLoading = true
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            isLoading = false
+            shots = parseShots(json: [:])
+            showShotList = true
+        }
+    }
+
+    // 解析分镜的数据
+    func parseShots(json: [String: Any]) -> [ShotListView.Shot] {
+        // 这里只是一个示例，你需要根据你的数据格式来实现这个功能
+        return MockData.mockShotLists
     }
 }
