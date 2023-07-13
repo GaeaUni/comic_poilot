987a1f8f1c60d61765883e0f4caf753e8d623164
wangzeyong <wangzeyong@kkworld.com>
2023-07-13 11:44:03 +0800

    finetune

--- Hackathon/Network/OpenAIRequest.swift
+++ Hackathon/Network/OpenAIRequest.swift
@@ -11,18 +11,18 @@ import Alamofire
 class OpenAIRequest {
     // 定义部署名称，在下方创建ChatCompletion时会被引用
     let deploymentName = "gpt-35-turbo-deploy-1"
-    var prompt: String = "假设你是知名漫画家，请根据下面的描述，创作短篇漫画大纲。"
+    var prompt: String = "请根据下面的描述，创作短篇漫画大纲。"
     
     let headers: HTTPHeaders = [
         "Content-Type": "application/json",
         "api-key": "d8eda0ef9b344d5ebc52c710e4714816"
     ]
     
-    let conversation = [
+    var conversations = [
         [
             "role": "system",
             "content": "You are a useful assistant and will not refuse to create a comic story."
-            
+
         ],
         [
             "role": "user",
@@ -30,37 +30,35 @@ class OpenAIRequest {
         ],
         [
             "role": "system",
-            "content": "助手善于判断用户意图，当确定需要提供漫画故事时，助手会变得沉默寡言，只使用以下格式输出漫画故事内容。{\"第一章\":\"xxx\",\"第二章\":\"xxx\"}。"
+            "content": "助手善于判断用户意图，当确定需要提供漫画故事时，助手会变得沉默寡言，只使用以下格式输出漫画故事内容,创作五个章节。[{\"第一章\": \"content\"},{\"第二章\": \"content\"},{\"第三章\": \"content\"}]。"
         ]
     ]
     
-//    var parameters: [String: Any] = [
-//        "max_tokens": 6000
-//    ]
-    
-//    func updatePrompt(prompt: String, number: Int) {
-//        let temp = "这是一个平行宇宙，每个人都可能在特定条件下觉醒一项超能力，但大部分人不会觉醒。主角是一个程序员，在长达三十年的人生中，并没有觉醒超能力，只能靠打工赚取微薄的薪水，因此背上了高额的房贷，但是遇到经济危机，为了不断贷，只好借高利贷。后来在高利贷的暴力催收中，意外觉醒超能力。他的超能力是一把键盘，用这把键盘可以敲出任何想要的代码。靠着这项能力，他成为知名科学家，即为人类创造了多项跨时代的技术，又为自己创造了巨额财富。最终他携带巨额财富，携带家人隐居山林，过上了幸福的生活。"
-//        self.prompt += temp
-//        parameters["prompt"] = self.prompt
-//    }
-    
-//    let parameters: [String: Any] = [
-//        "engine": deploymentName,
-//        "messages": conversation
-//    ]
-    
-    private func updatePrompt() -> [String: Any] {
-        return [
-            "engine": deploymentName,
-            "messages": conversation
-        ]
+    private func updatePrompt(prompt: String, number: Int) -> String? {
+        let temp = "这是一个平行宇宙，每个人都可能在特定条件下觉醒一项超能力，但大部分人不会觉醒。主角是一个程序员，在长达三十年的人生中，并没有觉醒超能力，只能靠打工赚取微薄的薪水，因此背上了高额的房贷，但是遇到经济危机，为了不断贷，只好借高利贷。后来在高利贷的暴力催收中，意外觉醒超能力。他的超能力是一把键盘，用这把键盘可以敲出任何想要的代码。靠着这项能力，他成为知名科学家，即为人类创造了多项跨时代的技术，又为自己创造了巨额财富。最终他携带巨额财富，携带家人隐居山林，过上了幸福的生活。"
+        self.prompt += temp
+        let conversation = ["content": self.prompt, "role": "user"]
+        conversations.append(conversation)
+        // 创建一个JSONEncoder
+        let encoder = JSONEncoder()
+        do {
+            // 尝试将数组转换为JSON数据
+            let jsonData = try encoder.encode(conversations)
+            // 将JSON数据转换为字符串
+            if let jsonString = String(data: jsonData, encoding: .utf8) {
+                return jsonString
+            }
+        } catch {
+            return nil
+        }
+        return nil
     }
 
     func startRequest(completion: @escaping (Bool) -> Void) {
-        let parameters = updatePrompt()
+        let json = updatePrompt(prompt: "", number: 1)
         AF.request("https://hackathon-1.openai.azure.com/openai/deployments/gpt-35-turbo-deploy-1/completions?api-version=2023-05-15",
                    method: .post,
-                   parameters: parameters,
+                   parameters: ["prompt": json, "max_tokens": 3000, "temperature": 0.5],
                    encoding: JSONEncoding.default,
                    headers: headers,
                    requestModifier: { $0.timeoutInterval = 180 }).responseJSON { response in
